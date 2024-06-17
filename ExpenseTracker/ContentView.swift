8091255fdb36bb9011205e8fc10054c886357a85
Onur Anıl <onuranil749@gmail.com>
2024-06-17 18:00:48 +0300

    Initial Commit

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Onur Anıl on 17.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
